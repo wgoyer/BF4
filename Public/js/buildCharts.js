96f03beb3929dec07c5855e3e75057c35055e59e
wgoyer <wgoyer@gmail.com>
2014-01-28 20:59:21 -0700

    Got the button toggles to work!

--- Public/js/buildCharts.js
+++ Public/js/buildCharts.js
@@ -1,4 +1,5 @@
 var createChart = function(chartData, divId){
+	console.log("Creating charts with: "+chartData+" and: "+divId);
 	AmCharts.ready(function () {
 		var graph = new AmCharts.AmGraph();
 		var chart = new AmCharts.AmSerialChart();
@@ -6,9 +7,28 @@ var createChart = function(chartData, divId){
 		chart.categoryField = "date";
 		graph.valueField = "difference";
 		graph.type = "column";
+		chart.angle = 30;
+		chart.depth3D = 15;
 		graph.fillAlphas = 0.5;
-		
+		graph.balloonText = "[[category]]: <b>[[value]]</b>";
 		chart.addGraph(graph);
 		chart.write(divId);
 	});
+};
+
+// The only reason this is in two functions is because this can't run after the page has loaded.
+// I can and should change this to a single function with an if clause and I will do it later.
+var createChartAfterLoad = function(chartData, divId){
+	var graph = new AmCharts.AmGraph();
+	var chart = new AmCharts.AmSerialChart();
+	chart.dataProvider = chartData;
+	chart.categoryField = "date";
+	graph.valueField = "difference";
+	graph.type = "column";
+	chart.angle = 30;
+	chart.depth3D = 15;
+	graph.fillAlphas = 0.5;
+	graph.balloonText = "[[category]]: <b>[[value]]</b>";
+	chart.addGraph(graph);
+	chart.write(divId);
 };
--- Public/js/flipCard.js
+++ Public/js/flipCard.js
@@ -1,23 +1,26 @@
 var showTwitch = function(user){
-	var uName = user.name;
-	var tName = user.twitchId;
+	var jsonUser = JSON.parse(user);
+	var uName = jsonUser.name;
+	jsonUser.rankImage = "";
+	var tName = jsonUser.twitchID;
 	var centerDiv = '#'+uName+' .cardData';
 	var buttonId = '#'+uName+' .btnToggle';
 	$(centerDiv).html('<object id="live_embed_player_flash" type="application/x-shockwave-flash" height="378" width="620" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel='+tName+'" bgcolor="#000000"><param name="allowFullScreen" value="true"/><param name="allowScriptAccess" value="always"/><param name="allowNetworking" value="all"/><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf"/><param name="flashvars" value="hostname=www.twitch.tv&amp;channel='+tName+'&amp;auto_play=true&amp;start_volume=25"/></object>');
 	$(buttonId).html('Stats');
-	$(buttonId).attr('onClick', 'showStats("'+uName+'","'+tName+'")');
+	$(buttonId).attr('onClick', "showStats('"+JSON.stringify(jsonUser)+"')");
 	$(buttonId).attr('name', 'stats');
 		
 };
 
 var showStats = function(user){
-	var uName = user.name;
-	var tName = user.twitchId;
+	var jsonUser = JSON.parse(user);
+	var uName = jsonUser.name;
+	var tName = jsonUser.twitchId;
 	var centerDiv = '#'+uName+' .cardData';
 	var buttonId = '#'+uName+' .btnToggle';
-	createChart(user.dailyStats);
+	createChartAfterLoad(jsonUser.dailyStats, jsonUser.name+'_cardData');
 	$(buttonId).html('Twitch');
-	$(buttonId).attr('onClick', 'showTwitch("'+uName+'","'+tName+'")');
+	$(buttonId).attr('onClick', "showTwitch('"+user+"')");
 	$(buttonId).attr('name', 'twitch');
 };
 
